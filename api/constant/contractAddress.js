565e7081468fb2c9c9219c46bd416ce68519ca66
RonexLemon <ronexondimu@gmail.com>
2024-04-07 21:29:46 +0300

    add constants

---
+++ api/constant/contractAddress.js
@@ -0,0 +1,10 @@
+const SimpleContractAddress = "0x7328867d289127984A9CDCdf1d0dC101Ae339320"
+
+// Deployer: 0x65E28C9C4Ef1a756d8df1c507b7A84eFcF606fd4
+// Deployed to: 0x7328867d289127984A9CDCdf1d0dC101Ae339320
+// Transaction hash: 0xa00004acd58a9dc75e1ebe405b717b09e09605fe0effbb12fd85e23ef2650621
+
+
+module.exports={
+    SimpleContractAddress
+}

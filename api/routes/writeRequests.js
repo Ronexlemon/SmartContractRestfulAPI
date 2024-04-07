d7ed3e1a9f416a2a5089f94e9c460dd0c1677aae
RonexLemon <ronexondimu@gmail.com>
2024-04-07 21:18:37 +0300

    write requests

--- api/routes/writeRequests.js
+++ api/routes/writeRequests.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/companyDetail/:id", async (req, res) => {
+router.post("/companyDetail/:id", async (req, res) => {
     try {
         const { id } = req.params;
         res.status(200).json({ data: id, msg: "success" });
@@ -10,7 +10,7 @@ router.get("/companyDetail/:id", async (req, res) => {
     }
 });
 
-router.get("/allcompanyDetails",async(req,res)=>{
+router.post("/allcompanyDetails",async(req,res)=>{
     try{
         
         res.status(200).json({msg:"success"});
--- api/server.js
+++ api/server.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const { ethers } = require("ethers");
 const getRequests = require("../api/routes/getRoutes")
+const writeRequests = require("../api/routes/writeRequests")
 
 const Port = 3000;
 const App = express();
 
 App.use(express.json());
 
-App.use("/api",getRequests);
+App.use("/api/read",getRequests);
+App.use("/api/write",getRequests);
 
 
-App.get("/cmp",async(req,res)=>{
-    try{
-        
-        res.status(200).json({msg:"success"});
-    }catch(error){
-        res.status(500).json({msg:error});
-    }
 
-})
 
 App.listen(Port, () => {
   console.log(`Server is running on port ${Port}`);
